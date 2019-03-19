4d66a2d9ab959896d4bae6b12efbe99efd6eb588
Atomz <gotshell@gmail.com>
2019-03-19 17:17:11 +0800

    no message

--- pac.js
+++ pac.js
@@ -1,25 +1,20 @@
 var direct = 'DIRECT';
 var httpProxy = 'PROXY';
 
-var directList = [
+var proxyList = [
 	"", // corresponds to simple host name and ip address
-	"taobao.com",
-	"www.baidu.com"
+	"cip.cc",
+	"ipip.net",
+	"95599.cn",
+	"abchina.com"
 ];
 
 var directAcc = {};
-for (var i = 0; i < directList.length; i += 1) {
-	directAcc[directList[i]] = true;
+for (var i = 0; i < proxyList.length; i += 1) {
+	directAcc[proxyList[i]] = true;
 }
 
 var topLevel = {
-        "ac": true,
-        "co": true,
-        "com": true,
-        "edu": true,
-        "gov": true,
-        "net": true,
-        "org": true
 };
 
 // hostIsIP determines whether a host address is an IP address and whether
@@ -91,92 +86,8 @@ function FindProxyForURL(url, host) {
 	}
 	var domain = host2Domain(host);
 	if (host.length == domain.length) {
-		return directAcc[host] ? direct : httpProxy;
+		return directAcc[host] ? httpProxy : direct;
 	}
-	return (directAcc[host] || directAcc[domain]) ? direct : httpProxy;
+	return (directAcc[host] || directAcc[domain]) ? httpProxy : direct;
 }
 
-// Tests
-
-var testData, td, i;
-
-testData = [
-	{ ip: '127.0.0.1', isIP: true, isPrivate: true },
-	{ ip: '127.2.1.1', isIP: true, isPrivate: true },
-	{ ip: '192.168.1.1', isIP: true, isPrivate: true },
-	{ ip: '172.16.1.1', isIP: true, isPrivate: true },
-	{ ip: '172.20.1.1', isIP: true, isPrivate: true },
-	{ ip: '172.31.1.1', isIP: true, isPrivate: true },
-	{ ip: '172.15.1.1', isIP: true, isPrivate: false },
-	{ ip: '172.32.1.1', isIP: true, isPrivate: false },
-	{ ip: '10.16.1.1', isIP: true, isPrivate: true },
-	{ ip: '12.3.4.5', isIP: true, isPrivate: false },
-	{ ip: '1.2.3.4.5', isIP: false, isPrivate: false },
-	{ ip: 'google.com', isIP: false, isPrivate: false },
-	{ ip: 'www.google.com.hk', isIP: false, isPrivate: false }
-];
-
-for (i = 0; i < testData.length; i += 1) {
-	td = testData[i];
-	arr = hostIsIP(td.ip);
-	if (arr[0] !== td.isIP) {
-		if (td.isIP) {
-			console.log(td.ip + " is ip");
-		} else {
-			console.log(td.ip + " is NOT ip");
-		}
-	}
-	if (arr[0] !== td.isIP) {
-		if (td.isIP) {
-			console.log(td.ip + " is private ip");
-		} else {
-			console.log(td.ip + " is NOT private ip");
-		}
-	}
-}
-
-testData = [
-	// private ip should return direct
-	{ host: '192.168.1.1', mode: direct},
-	{ host: '10.1.1.1', mode: direct},
-	{ host: '172.16.2.1', mode: direct},
-	{ host: '172.20.255.255', mode: direct},
-	{ host: '172.31.255.255', mode: direct},
-	{ host: '192.168.2.255', mode: direct},
-
-	// simple host should return direct
-	{ host: 'localhost', mode: direct},
-	{ host: 'simple', mode: direct},
-
-	// non private ip should return proxy
-	{ host: '172.32.2.255', mode: httpProxy},
-	{ host: '172.15.0.255', mode: httpProxy},
-	{ host: '12.20.2.1', mode: httpProxy},
-
-	// host in direct domain/host should return direct
-	{ host: 'taobao.com', mode: direct},
-	{ host: 'www.taobao.com', mode: direct},
-	{ host: 'www.baidu.com', mode: direct},
-
-	// host not in direct domain should return proxy
-	{ host: 'baidu.com', mode: httpProxy},
-	{ host: 'foo.baidu.com', mode: httpProxy},
-	{ host: 'google.com', mode: httpProxy},
-	{ host: 'www.google.com', mode: httpProxy},
-	{ host: 'www.google.com.hk', mode: httpProxy},
-
-	// host in local domain should return direct
-	{ host: 'test.local', mode: direct},
-	{ host: '.local', mode: direct},
-];
-
-for (i = 0; i < testData.length; i += 1) {
-	td = testData[i];
-	if (FindProxyForURL("", td.host) !== td.mode) {
-		if (td.mode === direct) {
-			console.log(td.host + " should return direct");
-		} else {
-			console.log(td.host + " should return proxy");
-		}
-	}
-}
